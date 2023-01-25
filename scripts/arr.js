1d674e97550026aa136cf0ef4ecf9e4c4608f803
KEN ROE <shdnrnjs@gmail.com>
2023-01-25 12:14:47 +0900

    폭탄섞기

--- scripts/arr.js
+++ scripts/arr.js
@@ -1,31 +1,65 @@
 /* 배열 선언 */
- 
+let arr = [] ;//이 방법을 많이 씀
+let arr2 = new Array() ; 
 
 /* 배열 추가 삭제 :뒤에서 */
+arr.push('🍕');// push : 맨처음 들어간게 맨 밑에 저장
+arr.push('🍔');//이모지는 .+window키
+arr.push('🍟');
+console.log(arr);
+//push pop 많이 사용
+
+arr.pop();//pop - 뒤에서 하나 제거
+console.log(arr);
+
  
 /* 배열 추가 삭제 :앞에서 */
-  
+arr.unshift('🤣');//뒤에꺼 밀고 맨 앞에 삽입
+arr.unshift('😁');
+console.log(arr);
 
-/* 배열 순회 */
- 
+arr.shift();//맨 앞에꺼 제거
+console.log(arr);
+
+
+
+/* 배열 순회1 */
+//하나씩 접근해서 쓴다
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i]);
+} 
+
+/* 배열 순회2 */
+//for of 구문
+for(let i of arr){
+    console.log(i);
+} 
+
 /* 배열의 length 속성*/
- 
-
-//배열 비우기
- 
+console.log(arr);
+console.log(arr.length) ; 
+//배열 비우기 //속성값은 private으로 설정됨.자스에서 length는 속성인데 변경가능
+arr.length = 0;
+console.log(arr);
 
 /* 배열을 문자열로 반환*/
-arr = ['🍕','🍔','🍗','🍜',] ;
- 
-/* 문자열 split하면 배열반환*/
- 
+arr = ['🍕','🍔','🍗','🍜',] ; //배열 마지막 ',' : 텔링콤마. 오류안남. 배열이 변경될 수 있다는 의미
+let s = arr.toString();
+console.log(s); 
+
+// /* 문자열 split하면 배열반환*/
+let sarr = s.split(',');
+console.log(sarr);
+
+
 /* 배열 정렬 */
 let num = [1,3,5,2,4];
- 
+num.sort((a, b) => {return b-a;}) ;//내림차순 오름차순 할 때 콜백함수 이용하여 작성
+console.log(num); 
 
 /* 배열 섞기*/
- 
- 
+num.sort(() => Math.random() - 0.5)
+console.log(num); 
 
 /* filter : 주어진 함수의 조건을 만족하는 요소를 모아 새로운 배열로 반환*/
 //3보다 큰수 집합 
@@ -35,17 +69,23 @@ let setA = [1,2,3,4,5];
 let setB = [4,5,6,7,8];
 
 //합집합
-let union =  
+let union = [...setA, ...setB]//두 배열을 펼쳐서 하나의 배열로 만듦 
 console.log(union);
 
 //교집합
-let intersection =  
+let intersection = setA.filter((item) => setB.includes(item))//filter는 골라서 가져옴  
 console.log(intersection);
 
+let setA2 = setA.map((item) => item * 2 )//map은 다 가져오는데 어떻게 변형시켜 가져올건지
+console.log(setA2);
+
 //차집합
-let difference =  
+let difference = setA.filter((item) => !setB.includes(item))
 console.log(difference);
 
+//합집합 수정
+union = [...setA, ...setB] 
+console.log(union);
 
 
 
--- scripts/str.js
+++ scripts/str.js
@@ -20,7 +20,7 @@ for(let n of st){
 }
 console.log(sum);
 
-/* 여러줄 문자열 저장 */
+/* 여러줄 문자열 저장 - 백틱사용 */
 s = `Ecma International의 프로토타입 기반의 프로그래밍 언어로, 스크립트 언어에 해당된다.
 특수한 목적이 아닌 이상 모든 웹 브라우저에 인터프리터가 내장되어 있다. 
 오늘날 HTML, CSS와 함께 웹을 구성하는 요소 중 하나다.
@@ -28,10 +28,35 @@ HTML이 웹 페이지의 기본 구조를 담당하고, CSS가 디자인을 담�
 JavaScript는 클라이언트 단에서 웹 페이지가 동작하는 것을 담당한다.
 웹 페이지를 자동차에 비유하자면, HTML은 자동차의 뼈대, CSS는 자동차의 외관, 
 JavaScript는 자동차의 동력이라고 볼 수 있다.`
+console.log(s);
 
  
-/*실습 : 위의 문자열에서 HTML은 몇번 사용되었는지 확인*/
- 
+/*실습 : 위의 문자열에서 HTML은 몇번 사용되었는지 확인 - 구글링*/
+let pos = 0;
+let cnt = 0;
+
+//for는 끝이 정해져있을 때, while은 언제 끝날 지 모를 때 사용
+while (true) {
+    pos = s.indexOf("웹", pos);
+    if (pos == -1) break;
+
+    cnt++;
+    pos = pos + 1 ;
+}
+console.log(cnt);
+console.log(pos);
+
 
 /* 문자열 슬라이싱*/
- 
+let w = s.substr(0, 4)//0번째 부터 4개 자름
+console.log(w);
+
+w = s.substring(5, 10);//5번 째부터 10번째까지
+console.log(w);
+
+w = s.slice(-4);//substring과 같으나 차이는 마이너스 값 가능. -4번째부터 끝까지 출력
+console.log(w);
+
+w = s.slice(4);//4번 째부터 끝까지 출력
+console.log(w);
+
